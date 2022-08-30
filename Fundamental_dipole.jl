f9b73b38dde21c0d1d704384db28cfc29b556b82
HaowuDuan <42245515+HaowuDuan@users.noreply.github.com>
2022-08-30 09:54:39 -0400

    wilson line computed

--- Fundamental_dipole.jl
+++ Fundamental_dipole.jl
@@ -12,81 +12,105 @@ using Distributions
 using FFTW
 using Plots
 
+
+# This file is a test of calculation of the dipole correlator in MV model.
+# All calculation will be done at fixed parameters.
+#Once the code is working, it will be modifined and put into a bigger CGC package where the functions can be called.
 #pre-defined functions
-# SU(3) generators
+#SU(3) generators, t[a, i, j] is 8*3*3 matrix, T[a,b,c] is 8*8*8 matrix.
 include("SU3.jl")
 
-# The parameters
 
 #longitudinal layers
-Ny=6
+Ny=100
 # Transverse lattice size in one direction
 N=64
 # lattice spacing
-a=0.1
+a=1
 # infra regulator m
-m=0.01
-# Saturation momentum for fixed coupling
-Qs=1
-#the coupling constant g
-g_s=0.15
-# the strong coupling constant
-αs=g_s^2/(4*pi)
-# color charge density mu^2
-μ2=1
+m=2/N
+#
+gμ=1
 
-rng=Random.seed!(123)
+# define the correct wave number
+wave_number=fftfreq(N,2π)
 
-function V(Ny,μ2,N,m,g_s,a)
-    ρ_x=rand(Normal(0,sqrt(μ2)),Ny,N,N,8)
-    ρ_k=randn(rng, ComplexF64, (Ny,N,N,8))
+# Calculate magnitude of lattice momentum square for later use
 
+K2=zeros(N,N)
+for i in 1:N, j in 1:N
+    K2[i,j]=2(2-cos(wave_number[i])-cos(wave_number[j]))
+end
 
-    V_momentum=randn(rng, ComplexF64, (Ny,N,N,3,3))
+#calculate fft and ifft plans for later use
+rho=randn(ComplexF32, (N,N))
+fft_p=plan_fft(rho; flags=FFTW.MEASURE, timelimit=Inf)
+ifft_p=plan_ifft(rho; flags=FFTW.MEASURE, timelimit=Inf)
 
-    for i in 1:Ny
-        for a in 8
-            ρ_k[i,:,:,a]=fftshift(fft(ρ_x[i,:,:,a]))
-        end
+function V()
+
+    ρₓ=rand(Normal(0,gμ/sqrt(Ny)),Ny,N,N,8) # draw the color charge density from N(0,1)
+                                    # for each layer, each point, and each color
+
+    ρₖ=randn(ComplexF32, (Ny,N,N,8)) # create a random complex matrix to store the data from FFT
+
+    Aₖ=randn(ComplexF32, (Ny,N,N,8)) # create a random complex matrix to store the data for momemtum space field for fixed color
+    Aₓ=similar(Aₖ)                   # same thing for coordinate space
+
+    Vₓ=randn(ComplexF32, (N,N,3,3))
+    #calculate ρₖ
+    for i in 1:Ny, a in 1:8
+            ρₖ[i,:,:,a]=fft(ρₓ[i,:,:,a])
+    end
+    #calculate A(k) for fixed color
+    for n in 1:N, l in 1:N
+        Aₖ[:,n,l,:]=ρₖ[:,n,l,:]/(K2[n,l]+m^2)
+    end
+    # calculate A(x). Note that A(x) is real
+    for i in 1:Ny, a in 1:8
+        Aₓ[i,:,:,a]=real(ifft(Aₖ[i,:,:,a]))
+    end
+    # taking the product of all layer
+    for n in 1:N, l in 1:N
+         Vₓ[n,l,:,:]=exp(sum(-im*Aₓ[1,n,l,a]*t[a] for a in 1:8))
+         for i in 2:Ny
+             Vₓ[n,l,:,:]=exp(sum(-im*Aₓ[i,n,l,a]*t[a] for a in 1:8))*Vₓ[n,l,:,:]
+         end
     end
 
-    for i in 1:Ny
-        for kx in 1:N
-            for ky in 1:N
-                  V_momentum[i,kx,ky,:,:]=exp.(-im*g_s*sum(ρ_k[i,kx,ky,j]*t[j] for j in 1:8)/((sin(kx*2pi*a/(N*2)-a*pi)^2+sin(ky*2pi*a/(N*2)-a*pi)^2-a*pi)*4/a^2+m^2))
-            end
+    @time Vₓ
+end
+# using fft and ifft plans, keep it for later testing
+function V_plan()
+
+    ρₓ=rand(Normal(0,gμ/sqrt(Ny)),Ny,N,N,8) # draw the color charge density from N(0,1)
+                                    # for each layer, each point, and each color
+
+    ρₖ=randn(ComplexF32, (Ny,N,N,8)) # create a random complex matrix to store the data from FFT
+
+    Aₖ=randn(ComplexF32, (Ny,N,N,8)) # create a random complex matrix to store the data for momemtum space field for fixed color
+    Aₓ=similar(Aₖ)                   # same thing for coordinate space
+
+    Vₓ=randn(ComplexF32, (N,N,3,3))
+    #calculate ρₖ
+    for i in 1:Ny, a in 1:8
+            ρₖ[i,:,:,a]=fft_p*ρₓ[i,:,:,a]
+    end
+    #calculate A(k) for fixed color
+    for n in 1:N, l in 1:N
+        Aₖ[:,n,l,:]=ρₖ[:,n,l,:]/(K2[n,l]+m^2)
+    end
+    # calculate A(x). Note that A(x) is real
+    for i in 1:Ny, a in 1:8
+        Aₓ[i,:,:,a]=real(ifft_p*Aₖ[i,:,:,a])
+    end
+    # taking the product of all layer
+    for n in 1:N, l in 1:N
+         Vₓ[n,l,:,:]=exp(sum(-im*Aₓ[1,n,l,a]*t[a] for a in 1:8))
+         for i in 2:Ny
+             Vₓ[n,l,:,:]=exp(sum(-im*Aₓ[i,n,l,a]*t[a] for a in 1:8))*Vₓ[n,l,:,:]
          end
-     end
+    end
 
-    V_momentum
+    @time  Vₓ
 end
-
-# Given the result of fundamental wilson_line, we define the fundamental dipole as
-function dipole_f_momentum(Ny,μ2,N,m,g_s,a)
-        W=V(Ny,μ2,N,m,g_s,a)
-        dipole=randn(rng, ComplexF64, (N,N))
-        for kx in 1:N
-            for ky in 1:N
-               dipole[kx,ky]=prod(tr(transpose(conj(W[i,kx,ky,:,:]))*W[i,kx,ky,:,:]) for i in 1:Ny)
-           end
-       end
-
-   @time   dipole
-end
-
-
-tmp=V(Ny,μ2,N,m,g_s,a)
-
-
-data_dipole_f=randn(rng, ComplexF64, (100,N,N))
-
-for i in 1:100
-    tmp=dipole_f_momentum(1,μ2,N,m,g_s,a)
-    data_dipole_f[i,:,:]=tmp
-end
-
-data_dipole=sum(data_dipole_f[i,:,:] for i in 1:100)/100
-
-
-
-scatter(1:64 , data_dipole[32,:])
