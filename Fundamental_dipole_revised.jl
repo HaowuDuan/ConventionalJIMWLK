62d68090c8a7203252c4bb2d000d17ec4e1016dc
HaowuDuan <42245515+HaowuDuan@users.noreply.github.com>
2023-03-10 15:56:03 -0500

    dipole fixed

--- Fundamental_dipole_revised.jl
+++ Fundamental_dipole_revised.jl
@@ -15,7 +15,6 @@ using JLD2
 using FileIO
 using ProgressMeter
 using BenchmarkTools
-using GellMannMatrices
 
 # This file is a test of calculation of the dipole correlator in MV model.
 # All calculation will be done at fixed parameters.
@@ -25,21 +24,20 @@ using GellMannMatrices
 #now updated to include the identity matrix t[9]=I/3
 include("SU3.jl")
 #longitudinal layers
-Ny=50
+const Ny=50
 # Transverse lattice size in one direction
-N=64*4
+const N=64*4
 #lattice size
-L=32
+const L=32
 # lattice spacing
-a=L/N
+const a=L/N
 # infra regulator m
-m2=(0.001)^2#1/L
+const m2=(0.001)^2#1/L
 #
-gμ=1
-
+const gμ=1
 #
-Nc=3
-Ng=Nc^2-1
+const Nc=3
+const Ng=Nc^2-1
 # number of configurations
 #N_config=100
 # anything will be updated with new N
@@ -76,43 +74,20 @@ function rho_k()
     fft_p*rand(rng,Normal(0,gμ/(sqrt(Ny)*a)),N,N)# draw the color charge density from N(0,1)                                # for each layer, each point, and each color
 end
 
-rho_test=@time rho_k()
-
-rho_test
 #function to compute field A(x) for a fixed color
-function compute_field!(rhok)
-# Modifies the argument to return the field
-    Threads.@threads for j in 1:N
-        for i in 1:N
-            @inbounds rhok[i,j] = a^2*rhok[i,j] / (a^2 * m2 + 4.0 * sin(π*(i-1)/N)^2 + 4.0 * sin(π*(j-1)/N)^2)
-            # factor of a^2 was removed to account for the normalization of ifft next
-            # ifft computes sum / (lenfth of array) for each dimension
-        end
-    end
-    rhok[1,1] = 0.0im
-    ifft!(rhok)
-end
+function Field(ρ)
 
-function Field!(ρ)
-        #A_k=similar(ρ)
         Threads.@threads for l in 1:N
             for n in 1:N
-            @inbounds ρ[n,l]=a^2*ρ[n,l]/(a^2 * m2 + 4.0 * sin(π*(n-1)/N)^2 + 4.0 * sin(π*(l-1)/N)^2) #(K2[n,l].+m2)#(2(2-cos(wave_number[n])-cos(wave_number[l]))/a^2 .+m^2 )
+            ρ[n,l]=ρ[n,l]/(K2[n,l].+m2)
             end
         end
-        # This is the problem 1
+        # This is the problem !!!!!!!!
         ρ[1,1] = 0.0im
 
-        #real.(ifft!(ρ))
-        ifft!(ρ)
+      real.(ifft_p*ρ)
 end
 
-
-BenchmarkTools.DEFAULT_PARAMETERS.samples = 5
-
-@benchmark Field!(rho_test)
-
-@benchmark compute_field!(rho_test)
 #@btime Field()
 #function to compute a single layer wilson line
 function Wilson_line()
@@ -140,18 +115,21 @@ end
 W_test=@time Wilson_line()
 
 #taking product for multiple layers
+
 function Wilson_line_Ny()
-    V=Wilson_line()
+    p=Progress(Ny)
+     V=Wilson_line()
      Threads.@threads for ny in 1:Ny-1
-         display(ny)
               W=Wilson_line()
            for l in 1:N, n in 1:N
               V[n,l,:,:]=W[n,l,:,:]* V[n,l,:,:]
             end
+            next!(p)
         end
       V
 end
 
+data_wilson_path=@time Wilson_line_Ny()
 
 # momentum space Wilson_line
 function V_k!(V)
@@ -233,129 +211,3 @@ function BOOTSTRAP(data,N_of_config)
      bootstrap_STD = std(bootstrap)
      bootstrap_MEAN,bootstrap_STD
 end
-
-
-#=
-
-function D_r()
-         dipole_tmp=D_xy()
-         dipole_r_tmp=zeros(r_size,2)
-         Threads.@threads for y2 in 1:N
-             for y1 in 1:N,x2 in 1:N,x1 in 1:N
-              x=[x1,x2]
-              y=[y1,y2]
-              i=r(x,y)
-
-              if i<r_size
-
-                   dipole_r_tmp[i+1,2]=dipole_r_tmp[i+1,2]+dipole_tmp[x1,x2,y1,y2]
-                   dipole_r_tmp[i+1,1]=dipole_r_tmp[i+1,1]+1
-              end
-            end
-        end
-
-
-        Threads.@threads for i in 1:r_size
-                dipole_r_tmp[i,2]=dipole_r_tmp[i,2]/dipole_r_tmp[i,1]
-                dipole_r_tmp[i,1]=i-1
-        end
-        dipole_r_tmp
-end
-
-
-
-p = Progress(1)
-
-for i in 1:N_config
-      data_tmp=D_r()
-      data_dipole_64[i,:,:]=data_tmp[:,:]
-      next!(p)
-end
-
-
-data_test=D_r()
-data_test20=D_r()
-scatter(data_test[:,1],data_test[:,2])
-
-
-scatter!(data_dipole_64[80,:,1],data_dipole_64[62,:,2])
-scatter(data_dipole_64[1,:,1],data_dipole_64[1,:,2])
-
-data_dipole_64_BS=zeros(40,3)
-
-for n in 1:40
-    data_dipole_64_BS[n,1]=n-1
-    data_tmp=data_dipole_64[:,n,2]
-    tmp=BOOTSTRAP(data_tmp,N_config)
-
-    data_dipole_64_BS[n,2]=tmp[1]
-    data_dipole_64_BS[n,3]=tmp[2]
-end
-
-
-scatter(data_dipole_64_BS[:,1],data_dipole_64_BS[:,2],yerr=data_dipole_64_BS[:,3])
-
-function BOOTSTRAP(data,N_of_config)
-     bootstrap=zeros(N_of_config)
-     for i in 1:N_of_config
-     BS_sample = similar(bootstrap)
-     for j in 1:N_of_config
-     index=rand(1:N_of_config)
-     BS_sample[j]=data[index]
-     end
-     bootstrap[i]=mean(BS_sample)
-     end
-
-     bootstrap_MEAN = mean(bootstrap)
-     bootstrap_STD = std(bootstrap)
-     bootstrap_MEAN,bootstrap_STD
-end
-
-
-writedlm("Data/fundamental_dipole_64_BS.dat",data_dipole_64_BS)
-
-save("Data/fundamental_dipole_64_raw.jld2", "D_64", data_dipole_64)
-
-# N=128
-data_dipole_128=zeros(100,r_size*2,2)
-for i in 1:100
-    data_dipole_128[i,:,:]=readdlm("Data/data/Dipole_$(i)_128.dat",Float64)
-end
-
-data_dipole_128_BS=zeros(80,3)
-
-
-for n in 1:80
-    data_dipole_128_BS[n,1]=n-1
-    data_tmp=data_dipole_128[:,n,2]
-    tmp=BOOTSTRAP(data_tmp,N_config)
-
-    data_dipole_128_BS[n,2]=tmp[1]
-    data_dipole_128_BS[n,3]=tmp[2]
-end
-
-scatter(data_dipole_64_BS[:,1],data_dipole_64_BS[:,2],yerr=data_dipole_64_BS[:,3], label="N=64")
-scatter!(data_dipole_128_BS[:,1].*0.5,data_dipole_128_BS[:,2],yerr=data_dipole_128_BS[:,3],label="N=128")
-
-
-
-writedlm("Data/fundamental_dipole_128_BS.dat",data_dipole_128_BS)
-
-save("Data/fundamental_dipole_128_raw.jld2", "D_128", data_dipole_128)
-
-
-f(x)=exp(-x^2/(10^2)/2)
-y=LinRange(0,40,100)
-Y=similar(y)
-Y.=f.(y)
-plot!(y,Y,label="y=exp(-(x/10)^2/2)")
-
-
-
-# cross check
-G=gellmann(Nc,skip_identity=false)
-
-G[9]
-
-
-=#
